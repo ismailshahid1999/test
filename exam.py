0c2fafbbbee0627a996a0be455d85183713bbe42
ismailshahid001 <ismailshahid>
2022-03-22 20:09:47 +0530

    added changes

--- exam.py
+++
@@ -1,9 +0,0 @@
-def equationSum(N) : 
-    
-    #Enter your code here
-    sum = 0
-    t = 2*N + (N+1)^2 - (N^2)+4
-    sum = sum + t
-    return sum
-
-print(equationSum(2))
--- java.js
+++
@@ -1,4 +0,0 @@
-// 
-num1 = prompt("enter the no :");
-num2 = prompt("enter another no :");
-alert("the sum is :"+ parseFloat(num1)+parseFloat(num2));
